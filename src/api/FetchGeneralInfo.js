ebf883af7c4784d067f9c460e837a78598d841c4
PiGodzyh <237793303@qq.com>
2024-10-17 22:21:34 +0800

    删去多余代码

--- src/api/FetchGeneralInfo.js
+++ src/api/FetchGeneralInfo.js
@@ -44,51 +44,6 @@ async function getSummary(content) {
     }
 }
 
-async function extractFirstSection(str) {
-    const match = str.match(/==经济==\n+([\s\S]+?)(={2}|$)/);
-    if (match) {
-        const economicParagraph = match[1].trim();
-        return economicParagraph;
-    }
-    // 如果没有找到匹配项，返回空字符串
-    return '';
-}
-
-async function getEconomy(content) {
-    let preChar = "";
-    let summary = "";
-    let isText = true;
-    for (let char of content) {
-        if (isText) {
-            if (char == "[" || char == "]" || char == "'" || char == "{" || char == "}" || char == "<" || char == ">");//去除[]'三个格式符号
-            else {
-                summary += char;
-            }
-        }
-        else {
-            // 格式部分结束
-            if (preChar == "}" && char == "}" || char == ">") {
-                isText = true;
-            }
-        }
-        /*
-        // 最后一个"}\n"作为开始标志
-        if (preChar == "}" && char == "\n") {
-            summary = "";
-        }
-        // 连续两个等号作为结束标志
-        if (preChar == "=" && char == "=") {
-            return summary.slice(0, -2);
-        }
-        */
-        // 格式部分判断
-        if (preChar == "{" && char == "{" || char == "<") {
-            isText = false;
-        }
-        preChar = char;
-    }
-    return summary;
-}
 /**
  * 功能：从API获取数据。返回gbk编码的简介
  * 参数：cityName
@@ -109,11 +64,7 @@ export async function fetchGeneralInfo(cityName) {
     try {
         const response = await axios.get(url, { params }); // 使用 await 等待 Promise 解决
         const content = response.data.query.pages[0].revisions[0].slots.main.content;
-        const summary = await getSummary(content);
-        const economicParagraph = await extractFirstSection(content);
-        console.log(economicParagraph);
-        const economy = await getEconomy(economicParagraph);
-        console.log(economy);
+        const summary = await getSummary(content);//简单处理获得简介
         return summary; // 返回处理后的数据
     } catch (error) {
         // 处理错误情况
